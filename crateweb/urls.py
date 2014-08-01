ab4fadda237aed5ac74779703849b4f84b3588ac
Matthias Wahl <matthias@crate.io>
2014-08-01 17:10:59 +0200

    remove packages prefix in urls

--- crateweb/urls.py
+++ crateweb/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
-from django.views.generic.simple import direct_to_template
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,20 +8,18 @@ import jutils.ji18n.translate
 jutils.ji18n.translate.patch()
 
 from crate.web.search.views import Search
-
+from crate.web.packages.views import ReleaseDetail
 
 handler500 = "pinax.views.server_error"
 
 
 urlpatterns = patterns("",
     url(r"^$", Search.as_view(), name="home"),
-#    url(r"^admin/", include(admin.site.urls)),
-#    url(r"^admin_tools/", include("admin_tools.urls")),
-    url(r"^packages/", include("crate.web.packages.urls")),
     url(r"^users/", include("crate.web.lists.urls")),
 
     url(r"^stats/", include("crate.web.packages.stats.urls")),
     url(r"^externally-hosted/$", "crate.web.packages.views.fuck_the_status_quo"),
+    url(r"^(?P<package>[^/]+)/(?:(?P<version>[^/]+)/)?$", ReleaseDetail.as_view(), name="package_detail"),
     url(r"^", include("crate.web.search.urls")),
 )
 
